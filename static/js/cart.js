b39c4cc6afa35caec8dfc92a1a1a3d412b1195a4
Sundar Khanal <sundarkhanal62@gmail.com>
2024-11-06 20:01:58 +0545

    add to cart vaxaina

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,34 +5,36 @@ for (i = 0; i < updateBtns.length; i++) {
 		var productId = this.dataset.product
 		var action = this.dataset.action
 		console.log('productId:', productId, 'Action:', action)
-		console.log('USER:', user)
 
-		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
-			
-		}else{
-			updateUserOrder(productId, action)
-		}
+        console.log('USER:', user)
+        if (user == 'AnonymousUser'){
+            console.log('User is not authenticated')
+                    
+        }else{
+            console.log('User is authenticated, sending data...')
+        }
+
 	})
 }
 
-function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+function updateUserOrder(productId, action)
+{
+    console.log('User is logged in,  sending data..')
+    var url = '/update_item/'
 
-		var url = '/update_item/'
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
 
-		fetch(url, {
-			method:'POST',
-			headers:{
-				'Content-Type':'application/json',
-				'X-CSRFToken':csrftoken,
-			}, 
-			body:JSON.stringify({'productId':productId, 'action':action})
-		})
-		.then((response) => {
-		   return response.json();
-		})
-		.then((data) => {
-		    location.reload()
-		});
+        },
+        body:JSON.stringify({productId, 'action':action})
+    })
+    .then((response) =>{
+        return response.json()
+    })
+    .then((data) =>{
+        console.log('Data:', data)
+    })
 }
